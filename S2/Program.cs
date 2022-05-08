6fd1f3e0258d24a64054882cb8bca43ae0577ba7
tarakavian84@gmail.com <tarakavian84@gmail.com>
2022-05-08 19:14:09 +0430

    Add project files.

---
+++ S2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int N = int.Parse(Console.ReadLine());
+            int max = N * 2;
+            Console.WriteLine("Multiply the number by 2: " + max);
+            Console.ReadKey();
+        }
+    }
+}
